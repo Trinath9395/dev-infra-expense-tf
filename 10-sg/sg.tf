75ad91c7ccc7fabff39a47be754d8b80dfd7d273
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-12 13:06:23 +0530

    terraform

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -48,6 +48,16 @@ module "app_alb_sg" {
   vpc_id         = data.aws_ssm_parameter.vpc_id.value
 }
 
+module "vpn_sg" {
+  source         = "git::https://github.com/Trinath9395/terraform-sg-module.git?ref=main"
+  project_name   = var.project_name
+  environment    = var.environment
+  sg_name        = "vpc"
+  common_tags    = var.common_tags
+  sg_description = "Created for VPN instances in expense"
+  vpc_id         = data.aws_ssm_parameter.vpc_id.value
+}
+
 resource "aws_security_group_rule" "app_alb_bastion" {
   type                     = "ingress"
   from_port                = 80
@@ -61,9 +71,46 @@ resource "aws_security_group_rule" "bastion_public" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
-  protocol          = "ssh"
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.bastion_sg.sg_id
 }
+
+resource "aws_security_group_rule" "vpn_ssh" {
+  type = "ingress"
+  from_port = 22 
+  to_port = 22 
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type = "ingress"
+  from_port = 443 
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type = "ingress"
+  from_port = 943 
+  to_port = 943
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type = "ingress"
+  from_port = 1194
+  to_port = 1194
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id 
+}
+
 
 
