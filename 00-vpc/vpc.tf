c93d8d9561a904d32ef1abb4b2957ed3b568ff4e
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-12 12:13:32 +0530

    terraform

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   #source                = "../terraform-vpc-module"
-  source = "git::https://github.com/Trinath9395/terraform-vpc-module.git?ref=main"
+  source                = "git::https://github.com/Trinath9395/terraform-vpc-module.git?ref=main"
   project_name          = var.project_name
   environment           = var.environment
   vpc_cidr              = var.vpc_cidr
@@ -9,5 +9,5 @@ module "vpc" {
   public_subnet_cidrs   = var.public_subnet_cidrs
   private_subnet_cidrs  = var.private_subnet_cidrs
   database_subnet_cidrs = var.database_subnet_cidrs
-  is_peering_required = true 
+  is_peering_required   = true
 }
