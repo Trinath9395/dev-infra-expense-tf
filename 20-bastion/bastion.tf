6dd27b49309f7105f52086466548f5f491a17bfc
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-12 12:14:12 +0530

    terraform

--- 20-bastion/bastion.tf
+++ 20-bastion/bastion.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion" {
-  ami                    = data.aws_ami.joindevops.id 
+  ami                    = data.aws_ami.joindevops.id
   vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
-  subnet_id = local.public_subnet_id
+  subnet_id              = local.public_subnet_id
   instance_type          = "t3.micro"
   tags = merge(
     var.common_tags,
--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,20 +1,20 @@
 data "aws_ami" "joindevops" {
-    most_recent      = true
-    owners           = ["973714476881"]
-    filter {
-        name   = "name"
-        values = ["RHEL-9-DevOps-Practice"]
-    }
+  most_recent = true
+  owners      = ["973714476881"]
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
 
-    filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-    }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
