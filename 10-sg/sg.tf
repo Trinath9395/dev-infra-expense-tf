e5c176198ff67c41be5e59c02a433ae3b0c3e34e
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-12 12:13:52 +0530

    terraform

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -39,7 +39,7 @@ module "bastion_sg" {
 }
 
 module "app_alb_sg" {
-  source =  "git::https://github.com/Trinath9395/terraform-sg-module.git?ref=main"
+  source         = "git::https://github.com/Trinath9395/terraform-sg-module.git?ref=main"
   project_name   = var.project_name
   environment    = var.environment
   sg_name        = "app-alb"
@@ -49,21 +49,21 @@ module "app_alb_sg" {
 }
 
 resource "aws_security_group_rule" "app_alb_bastion" {
-  type = "ingress"
-  from_port = 80 
-  to_port = 80 
-  protocol = "tcp"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
   source_security_group_id = module.bastion_sg.sg_id
-  security_group_id = module.app_alb_sg.sg_id
+  security_group_id        = module.app_alb_sg.sg_id
 }
 
 resource "aws_security_group_rule" "bastion_public" {
-  type = "ingress"
-  from_port = 22 
-  to_port = 22 
-  protocol = "ssh"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = module.bastion_sg.sg_id 
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "ssh"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.bastion_sg.sg_id
 }
 
 
