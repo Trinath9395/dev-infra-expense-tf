c9db1c8ff9aedc48ce100d3c208d7a11604ae62a
Trinath9395 <trinathdevops0606@gmail.com>
2026-01-13 15:00:00 +0530

    terraform

---
+++ 40-rds/variables.tf
@@ -0,0 +1,17 @@
+variable "common_tags" {
+  default = {
+    Project     = "expense"
+    Environment = "dev"
+    Terraform   = true
+  }
+}
+
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+
